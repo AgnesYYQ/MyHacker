f76c1d1ec446ce4e5a87be89b6d5228f0de6476e
agnesyyq <agnesyyq@gmail.com>
2020-10-01 10:04:18 -0600

    add lampshine and more exercises.

--- src/main/java/Codility.java
+++ src/main/java/Codility.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class Codility {
     public int leastUnoccuredInt(int[] A) {
@@ -77,9 +78,94 @@ public class Codility {
         return A[len - 1];
     }
 
+    public int jumpSteps(int X, int Y, int D) {
+        int distance = Math.abs(Y - X);
+        if (distance % D == 0) {
+            return distance / D;
+        } else {
+            int steps = distance / D + 1;
+            return steps;
+        }
+    }
+
+    public int findMissingInt(int[] A) {
+        if (A == null || A.length == 0) return 1;
+
+        Arrays.sort(A);
+        int len = A.length;
+
+        for (int i = 0; i < len; i++) {
+            if (A[i] > i + 1) return i + 1;
+        }
+        return A[len - 1] + 1;
+    }
+
+    public int minimalDifference(int[] A) {
+        if (A == null || A.length == 0) return 0;
+
+        int len = A.length;
+        if (A.length == 1) return A[0];
+
+        int preSum = A[0], sufSum = 0;
+        for (int i = 1; i < len; i++) {
+            sufSum += A[i];
+        }
+        int minDiff = Math.abs(sufSum - preSum);
+
+        for (int i = 1; i < len - 1; i++) {
+            preSum += A[i];
+            sufSum -= A[i];
+            minDiff = Math.min(minDiff, Math.abs(sufSum - preSum));
+        }
+
+        return minDiff;
+    }
+
+    public int frogRiverOne(int X, int[] A) {
+        if (A == null || A.length == 0) return -1;
+
+        HashSet<Integer> res = new HashSet<>();
+
+        int len = A.length;
+        for (int i = 0; i < len; i++) {
+            res.add(A[i]);
+            if (res.size() == X) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int[] maxCounter(int N, int[] A) {
+        int[] counters = new int[N];
+        int max = 0;
+        int lastUpdate = 0;
+        int len = A.length;
+
+        for (int i = 0; i < len; i++) {
+            if (A[i] == N + 1) {
+                lastUpdate = max;
+            } else {
+                int position = A[i] - 1;
+                if (counters[position] < lastUpdate) {
+                    counters[position] = lastUpdate + 1;
+                } else {
+                    counters[position]++;
+                }
+                max = Math.max(max, counters[position]);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            counters[i] = Math.max(counters[i], lastUpdate);
+        }
+
+        return counters;
+    }
+
     public static void main(String[] args) {
         Codility codility = new Codility();
-        int result = codility.maxGap(1041);
+        /*int result = codility.maxGap(1041);
         System.out.println(result);
 
         int[] input = {1, 2, 3, 4};
@@ -90,5 +176,16 @@ public class Codility {
         int unpaired = codility.unpaired(oddInput);
         System.out.println(unpaired);
 
+        int[] minDiffInput = {1000, -1000};
+        int minDiff = codility.minimalDifference(minDiffInput);
+        System.out.println(minDiff);
+
+        int[] frogRiverInput = {1, 3, 1, 4, 2, 3, 5, 4};
+        int frogJump = codility.frogRiverOne(5, frogRiverInput);
+        System.out.println(frogJump);*/
+
+        int[] maxCounterInput = {3, 4, 4, 6, 1, 4, 4};
+        int[] maxCount = codility.maxCounter(5, maxCounterInput);
+        System.out.println(Arrays.toString(maxCount));
     }
 }
---
+++ src/main/java/LampShine.java
@@ -0,0 +1,36 @@
+public class LampShine {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        if (A == null || A.length == 0) return 0;
+
+        int counts = 0;
+        int len = A.length;
+        boolean[] turnOns = new boolean[len];
+        int index = 0;
+
+        for (int i = 0; i < len; i++) {
+            int position = A[i] - 1;
+            turnOns[position] = true;
+            boolean allShine = true;
+            for (int j = index; j < position; j++) {
+                if (! turnOns[j]) {
+                    allShine = false;
+                    break;
+                }
+            }
+            if (allShine) {
+                index = position;
+                counts ++;
+            }
+        }
+
+        return counts;
+    }
+
+    public static void main(String[] args) {
+        LampShine test = new LampShine();
+        int[] input = {2, 1, 3, 5, 4};
+        int times = test.solution(input);
+        System.out.println(times);
+    }
+}
